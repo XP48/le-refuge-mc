b2182d12b85a4d7426758b14a819fb9ed83282af
Antonin Lancesseur <antoninlancesseur@gmail.com>
2024-09-25 19:13:14 +0200

    fonctionnalité copie

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,16 @@
 const ip = document.querySelector('.copy');
+const info = document.querySelector('.info');
+
+function resetInfo() {
+    info.innerText = "Cliquez ci-dessus pour copier l'ip";
+    info.style.color = "#f1f1f19c";
+}
 
 ip.addEventListener('click', () => {
     navigator.clipboard.writeText(ip.innerText);
+    info.innerText = "IP copié ✅";
+    info.style.color = "#12de6d";
+    setTimeout(() => {
+        resetInfo();
+      }, 1000);      
 });
